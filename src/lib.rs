57ff32aa74d5730585c3fbcf729c6ab3da906448
Justin Clift <justin@postgresql.org>
2020-03-17 21:00:35 +1100

    Start porting over the functions from my old tinygo_canvas2 project

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use js_sys::WebAssembly;
-use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 // use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader};
@@ -10,6 +9,13 @@ use wasm_bindgen::JsCast;
 //     program: web_sys::WebGlProgram,
 //     u_matrix: web_sys::WebGlUniformLocation,
 // }
+
+struct Point {
+    num: i32,
+    x:   f64,
+    y:   f64,
+    z:   f64,
+}
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
@@ -49,4 +55,82 @@ pub fn main() -> Result<(), JsValue> {
     context.stroke();
 
     Ok(())
+}
+
+// Transform the XYZ co-ordinates using the values from the transformation matrix
+fn transform(m: Vec<f64>, p: Point) -> Point {
+    let top0 = m[0];
+    let top1 = m[1];
+    let top2 = m[2];
+    let top3 = m[3];
+    let upper_mid0 = m[4];
+    let upper_mid1 = m[5];
+    let upper_mid2 = m[6];
+    let upper_mid3 = m[7];
+    let lower_mid0 = m[8];
+    let lower_mid1 = m[9];
+    let lower_mid2 = m[10];
+    let lower_mid3 = m[11];
+    //bot0 := m[12] // The fourth row values are ignored for 3D matrices
+    //bot1 := m[13]
+    //bot2 := m[14]
+    //bot3 := m[15]
+
+    Point {
+        num: p.num,
+        x: (top0 * p.x) + (top1 * p.y) + (top2 * p.z) + top3,
+        y: (upper_mid0 * p.x) + (upper_mid1 * p.y) + (upper_mid2 * p.z) + upper_mid3,
+        z: (lower_mid0 * p.x) + (lower_mid1 * p.y) + (lower_mid2 * p.z) + lower_mid3,
+    }
+}
+
+// Multiplies one matrix by another
+fn matrix_mult(opMatrix: Vec<f64>, m: Vec<f64>) -> Vec<f64>{
+    let top0 = m[0];
+    let top1 = m[1];
+    let top2 = m[2];
+    let top3 = m[3];
+    let upper_mid0 = m[4];
+    let upper_mid1 = m[5];
+    let upper_mid2 = m[6];
+    let upper_mid3 = m[7];
+    let lower_mid0 = m[8];
+    let lower_mid1 = m[9];
+    let lower_mid2 = m[10];
+    let lower_mid3 = m[11];
+    let bot0 = m[12];
+    let bot1 = m[13];
+    let bot2 = m[14];
+    let bot3 = m[15];
+
+    vec![
+    (opMatrix[0] * top0) + (opMatrix[1] * upper_mid0) + (opMatrix[2] * lower_mid0) + (opMatrix[3] * bot0), // 1st col, top
+    (opMatrix[0] * top1) + (opMatrix[1] * upper_mid1) + (opMatrix[2] * lower_mid1) + (opMatrix[3] * bot1), // 2nd col, top
+    (opMatrix[0] * top2) + (opMatrix[1] * upper_mid2) + (opMatrix[2] * lower_mid2) + (opMatrix[3] * bot2), // 3rd col, top
+    (opMatrix[0] * top3) + (opMatrix[1] * upper_mid3) + (opMatrix[2] * lower_mid3) + (opMatrix[3] * bot3), // 4th col, top
+
+    (opMatrix[4] * top0) + (opMatrix[5] * upper_mid0) + (opMatrix[6] * lower_mid0) + (opMatrix[7] * bot0), // 1st col, upper middle
+    (opMatrix[4] * top1) + (opMatrix[5] * upper_mid1) + (opMatrix[6] * lower_mid1) + (opMatrix[7] * bot1), // 2nd col, upper middle
+    (opMatrix[4] * top2) + (opMatrix[5] * upper_mid2) + (opMatrix[6] * lower_mid2) + (opMatrix[7] * bot2), // 3rd col, upper middle
+    (opMatrix[4] * top3) + (opMatrix[5] * upper_mid3) + (opMatrix[6] * lower_mid3) + (opMatrix[7] * bot3), // 4th col, upper middle
+
+    (opMatrix[8] * top0) + (opMatrix[9] * upper_mid0) + (opMatrix[10] * lower_mid0) + (opMatrix[11] * bot0), // 1st col, lower middle
+    (opMatrix[8] * top1) + (opMatrix[9] * upper_mid1) + (opMatrix[10] * lower_mid1) + (opMatrix[11] * bot1), // 2nd col, lower middle
+    (opMatrix[8] * top2) + (opMatrix[9] * upper_mid2) + (opMatrix[10] * lower_mid2) + (opMatrix[11] * bot2), // 3rd col, lower middle
+    (opMatrix[8] * top3) + (opMatrix[9] * upper_mid3) + (opMatrix[10] * lower_mid3) + (opMatrix[11] * bot3), // 4th col, lower middle
+
+    (opMatrix[12] * top0) + (opMatrix[13] * upper_mid0) + (opMatrix[14] * lower_mid0) + (opMatrix[15] * bot0), // 1st col, bottom
+    (opMatrix[12] * top1) + (opMatrix[13] * upper_mid1) + (opMatrix[14] * lower_mid1) + (opMatrix[15] * bot1), // 2nd col, bottom
+    (opMatrix[12] * top2) + (opMatrix[13] * upper_mid2) + (opMatrix[14] * lower_mid2) + (opMatrix[15] * bot2), // 3rd col, bottom
+    (opMatrix[12] * top3) + (opMatrix[13] * upper_mid3) + (opMatrix[14] * lower_mid3) + (opMatrix[15] * bot3)] // 4th col, bottom
+}
+
+// Translates (moves) a transformation matrix by the given X, Y and Z values
+fn translate(m: Vec<f64>, translate_x: f64, translate_y: f64, translate_z: f64) -> Vec<f64> {
+    let translate_matrix = vec![
+    1.0, 0.0, 0.0, translate_x,
+    0.0, 1.0, 0.0, translate_y,
+    0.0, 0.0, 1.0, translate_z,
+    0.0, 0.0, 0.0, 1.0];
+    matrix_mult(translate_matrix, m)
 }
